fd4a8211d4ae3742ff0deb5835de4c329f29a90e
lutherinbmore <40246808+lutherinbmore@users.noreply.github.com>
2018-07-17 13:49:09 -0400

    Update rhel_laptop.pp

--- site/role/manifests/rhel_laptop.pp
+++ site/role/manifests/rhel_laptop.pp
@@ -1,10 +1,4 @@
 class role::rhel_laptop {
   include profile::epel
 }
-file { '/home/lclarkjr/test.txt':
-  ensure => file,
-  content => "Welcome to Puppet!\n",
-}
-notify { 'greeting':
-  message => 'Welcome to puppet!'
-}
+
