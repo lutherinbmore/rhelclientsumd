9aed7b3ee1cb054592d3f53f05e3ee52f7942020
lutherinbmore <40246808+lutherinbmore@users.noreply.github.com>
2018-07-24 13:48:00 -0400

    Create init.pp

---
+++ site/python3/manifests/init.pp
@@ -0,0 +1,7 @@
+class python3 {
+  package { 'Python 3.6.5 (64-bit)':
+    ensure => installed,
+    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Python\python-3.6.5-amd64.exe'
+    install_options => ['/quiet','InstallAllUsers=1','PrependPath=1','Include_test=0'],
+    }
+}
