2b5c4940701ffab9e768bfbee5a8e683a7f77932
lutherinbmore <40246808+lutherinbmore@users.noreply.github.com>
2018-07-24 12:23:45 -0400

    Update init.pp

--- site/adobeccextras/manifests/init.pp
+++ site/adobeccextras/manifests/init.pp
@@ -1,7 +1,7 @@
 class adobeccextras {
   package { 'Photoshop':
     ensure => installed,
-    source => '\\ESSIWK170.ad.umd.edu\Deployment\Photoshop\Build\Photoshop.msi',
+    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Photoshop\Build\Photoshop.msi',
   }
   package { 'Illustrator':
     ensure => installed,
