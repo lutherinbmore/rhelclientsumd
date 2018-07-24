a1d530e4ca2482ea72ec3d96a179d15dfc015b35
lutherinbmore <40246808+lutherinbmore@users.noreply.github.com>
2018-07-24 11:11:50 -0400

    Update init.pp

--- site/adobeccextras/manifests/init.pp
+++ site/adobeccextras/manifests/init.pp
@@ -1,6 +1,6 @@
 class adobeccextras {
   package { 'Photoshop':
-    ensure => installedm
-    source => '\\ESSIWK170.ad.umd.edu\Deploy\Photoshop\Build\Photoshop.msi'
+    ensure => installed,
+    source => '\\ESSIWK170.ad.umd.edu\Deploy\Photoshop\Build\Photoshop.msi',
   }
 }
