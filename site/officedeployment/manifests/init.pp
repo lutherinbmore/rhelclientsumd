de79ba54598a121dd8f26814e33aaf746dcc57f7
Luther Clark <lclarkjr@umd.edu>
2018-07-31 11:17:52 -0400

    Added Acrobat, Google Chrome, and Mozilla Firefox to officedeployment module.

--- site/officedeployment/manifests/init.pp
+++ site/officedeployment/manifests/init.pp
@@ -3,4 +3,19 @@ class officedeployment {
     ensure => installed,
     source => '\\\ESSIWK170.ad.umd.edu\Deployment\Office Pro Plus 2016 64bit\setup.exe',
   }
+  package { 'Adobe Acrobat DC':
+    ensure => installed,
+    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Adobe Acrobat\Setup.exe',
+    install_options => ['/sall'],
+  }
+  package { 'Google Chrome':
+    ensure => installed,
+    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Google Chrome\googlechromestandaloneenterprise64.msi',
+    install_options => ['/qn'],
+  }
+  package { 'Mozilla Firefox 61.0.1 (x64 en-US)':
+    ensure => installed,
+    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Mozilla Firefox\Firefox Setup 61.0.1.exe',
+    install_options => ['-ms'],
+  }
 }
