a46c1c77274cc6d2477508f3e77c6414ec73829b
Luther Clark <lclarkjr@umd.edu>
2018-08-01 13:48:04 -0400

    Corrected DisplayName attribute for Python and the Adobe extras.

--- site/adobeccextras/manifests/init.pp
+++ site/adobeccextras/manifests/init.pp
@@ -1,9 +1,9 @@
 class adobeccextras {
-  package { 'Photoshop':
+  package { 'Adobe Photoshop CC 2018':
     ensure => installed,
     source => '\\\ESSIWK170.ad.umd.edu\Deployment\Photoshop\Build\Photoshop.msi',
   }
-  package { 'Illustrator':
+  package { 'Adobe Illustrator CC 2018':
     ensure => installed,
     source => '\\\ESSIWK170.ad.umd.edu\Deployment\Illustrator\Build\Illustrator.msi',
   }
--- site/python3/manifests/init.pp
+++ site/python3/manifests/init.pp
@@ -1,5 +1,5 @@
 class python3 {
-  package { 'Python 3.6.5 (64-bit)':
+  package { 'Python Launcher':
     ensure => installed,
     source => '\\\ESSIWK170.ad.umd.edu\Deployment\Python\python-3.6.5-amd64.exe',
     install_options => ['/quiet','InstallAllUsers=1','PrependPath=1','Include_test=0'],
