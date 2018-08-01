cc23e02a3527493708731c891a5bf59e09b46e3b
Luther Clark <lclarkjr@umd.edu>
2018-08-01 17:02:34 -0400

    Updated code to point to new deployment share.

--- site/adobeccextras/manifests/init.pp
+++ site/adobeccextras/manifests/init.pp
@@ -1,10 +1,10 @@
 class adobeccextras {
   package { 'Adobe Photoshop CC 2018':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Photoshop\Build\Photoshop.msi',
+    source => '\\\essi12.umd.edu\deployment-share\Photoshop\Build\Photoshop.msi',
   }
   package { 'Adobe Illustrator CC 2018':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Illustrator\Build\Illustrator.msi',
+    source => '\\\essi12.umd.edu\deployment-share\Illustrator\Build\Illustrator.msi',
   }
 }
--- site/officedeployment/manifests/init.pp
+++ site/officedeployment/manifests/init.pp
@@ -1,31 +1,31 @@
 class officedeployment {
   package { 'Microsoft Office Professional Plus 2016':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Office Pro Plus 2016 64bit\setup.exe',
+    source => '\\\essi12.umd.edu\deployment-share\Office Pro Plus 2016 64bit\setup.exe',
   }
   package { 'Adobe Acrobat DC':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Adobe Acrobat\Setup.exe',
+    source => '\\\essi12.umd.edu\deployment-share\Adobe Acrobat\Setup.exe',
     install_options => ['/sall'],
   }
   package { 'Google Chrome':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Google Chrome\googlechromestandaloneenterprise64.msi',
+    source => '\\\essi12.umd.edu\deployment-share\Google Chrome\googlechromestandaloneenterprise64.msi',
     install_options => ['/qn'],
   }
   package { 'Mozilla Firefox 61.0.1 (x64 en-US)':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Mozilla Firefox\Firefox Setup 61.0.1.exe',
+    source => '\\\essi12.umd.edu\deployment-share\Mozilla Firefox\Firefox Setup 61.0.1.exe',
     install_options => ['-ms'],
   }
   package { 'Cisco AnyConnect Secure Mobility Client ':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\Cisco AnyConnect\anyconnect-win-4.4.03034-core-vpn-predeploy-k9.msi',
+    source => '\\\essi12.umd.edu\deployment-share\Cisco AnyConnect\anyconnect-win-4.4.03034-core-vpn-predeploy-k9.msi',
     install_options => ['/norestart','/passive'],
   }
   package { 'SSH Tectia Client':
     ensure => installed,
-    source => '\\\ESSIWK170.ad.umd.edu\Deployment\TectiaClient\Setup.exe',
-    install_options => ['/s','/f1"\\\ESSIWK170.ad.umd.edu\Deployment\TectiaClient\setup.iss"'],
+    source => '\\\essi12.umd.edu\deployment-share\TectiaClient\Setup.exe',
+    install_options => ['/s','/f1"\\\essi12.umd.edu\deployment-share\TectiaClient\setup.iss"'],
   }
 }
