acf5f3eabaf7a21378f790e2d187a8f33830f289
Luther Clark <lclarkjr@umd.edu>
2018-08-02 17:22:29 -0400

    Corrected syntax errors in MATLAB resource declarations.

--- site/researchdeployment/manifests/init.pp
+++ site/researchdeployment/manifests/init.pp
@@ -1,10 +1,10 @@
 class researchdeployment {
-  package { 'MATLAB R2016b'
+  package { 'MATLAB R2016b':
   ensure => installed,
   source => '\\\essi12.umd.edu\deployment-share\MathWorks MATLAB 2016b\setup.exe',
   install_options => ['-inputFile my_installer_input.txt'],
   } ->
-  file { 'C:\Users\Public\Desktop\matlab.lnk'
+  file { 'C:\Users\Public\Desktop\matlab.lnk':
   ensure => file,
   source => 'puppet:///modules/researchdeployment/matlab.lnk'
   }
