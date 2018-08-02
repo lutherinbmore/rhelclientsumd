2b54bdcfa9d47bb92316ded079c4278a02aee661
Luther Clark <lclarkjr@umd.edu>
2018-08-02 17:02:52 -0400

    Added code to create a link to MATLAB on the public desktop.

--- site/researchdeployment/manifests/init.pp
+++ site/researchdeployment/manifests/init.pp
@@ -3,5 +3,9 @@ class researchdeployment {
   ensure => installed,
   source => '\\\essi12.umd.edu\deployment-share\MathWorks MATLAB 2016b\setup.exe',
   install_options => ['-inputFile my_installer_input.txt'],
+  } ->
+  file { 'C:\Users\Public\Desktop\matlab.lnk'
+  ensure => file,
+  source => 'puppet:///modules/researchdeployment/matlab.lnk'
   }
 }
