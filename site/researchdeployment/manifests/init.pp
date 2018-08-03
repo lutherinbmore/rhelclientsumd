8b9047d6a87687aa8aff16522c56fb345d157f79
Luther Clark <lclarkjr@umd.edu>
2018-08-03 12:04:11 -0400

    Deleted an extraneous space from the filepath for the install file. :-)

--- site/researchdeployment/manifests/init.pp
+++ site/researchdeployment/manifests/init.pp
@@ -2,7 +2,7 @@ class researchdeployment {
   package { 'MATLAB R2016b':
   ensure => installed,
   source => '\\\essi12.umd.edu\deployment-share\MathWorks MATLAB 2016b\setup.exe',
-  install_options => ['-inputFile',' \\\essi12.umd.edu\deployment-share\MathWorks MATLAB 2016b\my_installer_input.txt'],
+  install_options => ['-inputFile','\\\essi12.umd.edu\deployment-share\MathWorks MATLAB 2016b\my_installer_input.txt'],
   } ->
   file { 'C:\Users\Public\Desktop\matlab.lnk':
   ensure => file,
