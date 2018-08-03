196a713371babdb9dc7ae9899e5a34720cfc6511
Luther Clark <lclarkjr@umd.edu>
2018-08-03 16:11:34 -0400

    Added test of Exec resource.

--- site/arcgis10/manifests/init.pp
+++ site/arcgis10/manifests/init.pp
@@ -4,4 +4,14 @@ class arcgis10 {
   source => '\\\essi12.umd.edu\deployment-share\ArcGIS10.6\Setup.exe',
   install_options => ['ADDLOCAL=ALL','SOFTWARE_CLASS=Professional','SEAT_PREFERENCE=Fixed','DESKTOP_CONFIG=TRUE','/qb'],
   }
+  file {'c:\arctest.py':
+    ensure => file,
+    source => 'puppet:///modules/arcgis10/arctest.py',
+    source_permissions => ignore,
+  }
+  exec { 'python.exe c:\arctest.py':
+    path => 'C:\Python27\ArcGIS10.6\\',
+    require => File['c:\arctest.py'],
+    creates => 'C:\Program Files (x86)\ArcGIS\Desktop10.6\licensed',
+  }
 }
