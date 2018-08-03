737591a6e3d103d89594c2778b6b80720b467568
Luther Clark <lclarkjr@umd.edu>
2018-08-03 15:28:52 -0400

    Added Python test script for use in activating ArcGIS.

---
+++ site/arcgis10/files/arctest.py
@@ -0,0 +1,6 @@
+import arcpy
+licensed = arcpy.ProductInfo()
+if licensed == 'ArcInfo':
+    testfile = open('C:\Program Files (x86)\ArcGIS\Desktop10.6\licensed','w')
+    testfile.write(licensed)
+    licensed.close()
