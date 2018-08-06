71e6fa9283c13f30d9ecbcececec3728b462a35f
Luther Clark <lclarkjr@umd.edu>
2018-08-06 10:47:12 -0400

    Updated Exec and File resource declarations to run the PS activation script for ArcGIS.

--- site/arcgis10/manifests/init.pp
+++ site/arcgis10/manifests/init.pp
@@ -4,14 +4,14 @@ class arcgis10 {
   source => '\\\essi12.umd.edu\deployment-share\ArcGIS10.6\Setup.exe',
   install_options => ['ADDLOCAL=ALL','SOFTWARE_CLASS=Professional','SEAT_PREFERENCE=Fixed','DESKTOP_CONFIG=TRUE','/qb'],
   }
-  file {'c:\arctest.py':
+  file {'c:\ArcGISActivation.ps1':
     ensure => file,
-    source => 'puppet:///modules/arcgis10/arctest.py',
+    source => 'puppet:///modules/arcgis10/ArcGISActivation.ps1',
     source_permissions => ignore,
   }
-  exec { 'python.exe c:\arctest.py':
-    path => 'C:\Python27\ArcGIS10.6\\',
-    require => File['c:\arctest.py'],
+  exec { 'powershell.exe c:\ArcGISActivation.ps1':
+    path => 'C:\Windows\System32\WindowsPowerShell\v1.0\\',
+    require => File['c:\ArcGISActivation.ps1'],
     creates => 'C:\Program Files (x86)\ArcGIS\Desktop10.6\licensed',
   }
 }
